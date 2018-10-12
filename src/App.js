2a6e45a329e1cb6501ed4cdcea4b763beed610bc
AMutaa <piusandru@gmail.com>
2018-10-12 18:45:21 -0400

    update

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { createStore } from 'redux';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
+
+// const hello = () => ('hello');
+// const store = createStore(hello);
+// console.log(store);
